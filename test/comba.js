f1e671d7c7fa02994eec5c4de61db17d1063aff0
Kulagin Alex <alexkulagin@users.noreply.github.com>
2019-05-03 04:07:21 +0300

    added tests

---
+++ test/comba.js
@@ -0,0 +1,53 @@
+'use strict';
+
+
+//┐  COMBA
+//╠──███████████████████████████████████████████████████████████████████████████
+//┘
+
+
+	//┐  IMPORTS
+	//╠──⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙⁘⁙
+	//┘
+
+
+		// MODULES
+		// ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+			const { expect } = require('chai');
+			const { series, parallel } = require('../lib/Comba');
+
+
+
+
+//┐  TESTS
+//╠──░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+//┘
+
+
+	describe('COMBA TEST:\n', () =>
+	{
+		describe('interfaces:', () =>
+		{
+			describe('', () =>
+			{
+				it('series is function', () => expect(typeof series).to.equal('function'));
+				it('series return instance', () => expect(typeof series()).to.equal('object'));
+				it('series instance is Comba', () => expect(series().constructor.name).to.equal('Comba'));
+				it('series instance not equal series instance', () => expect(series()).to.not.equal(series()));
+			});
+
+			describe('', () =>
+			{
+				it('parallel is function', () => expect(typeof parallel).to.equal('function'));
+				it('parallel return instance', () => expect(typeof parallel()).to.equal('object'));
+				it('parallel instance is Comba', () => expect(parallel().constructor.name).to.equal('Comba'));
+				it('parallel instance not equal parallel instance', () => expect(parallel()).to.not.equal(parallel()));
+			});
+
+			describe('', () =>
+			{
+				it('series instance not equal parallel instance', () => expect(series()).to.not.equal(parallel()));
+			});
+		});
+	});
